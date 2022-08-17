556655be51b42d6485d4d43815eb517df9a1fff2
NotMeAgain10 <dev@isupport.com.gr>
2022-08-17 14:38:38 +0300

    pop login prin ta insert kai ta update

--- b2bProject/src/app/AdminArea/components/insert-users/insert-users.component.ts
+++ b2bProject/src/app/AdminArea/components/insert-users/insert-users.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import axios from "axios";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 export interface user {
   [index: number]: {
     afm: string,
@@ -20,25 +21,56 @@ export interface user {
 export class InsertUsersComponent implements OnInit {
   user :user |any =[];
   page:number = 1;
-  constructor() { }
+  showModal: boolean = false;
+  registerForm: FormGroup |any ;
+  submitted = false;
+  constructor(private formBuilder: FormBuilder) { }
+  show()
+  {
+    this.showModal = true; // Show-Hide Modal Check
 
-  ngOnInit(): void {
-      axios.get("https://perlaprodileapi.isupport.com.gr/main2.php?id=3/")
-        .then(resData=>{
-
-          for (let i = 0; i < resData.data.length; i++){
-
-            this.user[i]={
-              afm: resData.data[i].afm,
-              code: resData.data[i].code,
-              doy: resData.data[i].doy,
-              name: resData.data[i].name,
-              phone1: resData.data[i].phone1,
-              phone2: resData.data[i].phone2,
-              trdr : resData.data[i].trdr
-            }
-          }
-        })
   }
+  //Bootstrap Modal Close event
+  hide()
+  {
+    this.showModal = false;
+  }
+  ngOnInit() {
+    axios.get("https://perlaprodileapi.isupport.com.gr/main2.php?id=3/")
+      .then(resData=>{
+
+        for (let i = 0; i < resData.data.length; i++){
+
+          this.user[i]={
+            afm: resData.data[i].afm,
+            code: resData.data[i].code,
+            doy: resData.data[i].doy,
+            name: resData.data[i].name,
+            phone1: resData.data[i].phone1,
+            phone2: resData.data[i].phone2,
+            trdr : resData.data[i].trdr
+          }
+        }
+      })
+    this.registerForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+// convenience getter for easy access to form fields
+  get f() { return this.registerForm.controls; }
+  onSubmit() {
+    this.submitted = true;
+    // stop here if form is invalid
+    if (this.registerForm.invalid) {
+      return;
+    }
+    if(this.submitted)
+    {
+      this.showModal = false;
+    }
+
+  }
+
 
 }
