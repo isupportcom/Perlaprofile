90c9a104eed94d8d9f3b31449c76ae4c2cb87123
NotMeAgain10 <dev@isupport.com.gr>
2022-08-23 11:38:17 +0300

    new components in admin area

---
+++ b2bProject/src/app/AdminArea/components/insert-subcategory/insert-subcategory.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-insert-subcategory',
+  templateUrl: './insert-subcategory.component.html',
+  styleUrls: ['./insert-subcategory.component.css']
+})
+export class InsertSubcategoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
