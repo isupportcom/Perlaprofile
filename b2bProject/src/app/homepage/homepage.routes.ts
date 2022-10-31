cf748a7b7a4a5526c8496d2369c92e10e6016a0c
NotMeAgain10 <dev@isupport.com.gr>
2022-10-31 10:45:02 +0200

    asda

---
+++ b2bProject/src/app/homepage/homepage.routes.ts
@@ -0,0 +1,9 @@
+import { Route } from "@angular/router";
+import { HomepageComponent } from "./homepage.component";
+
+export const HomeRoutes : Route[] = [
+  {
+    path:'home',
+    component:HomepageComponent
+  }
+]
