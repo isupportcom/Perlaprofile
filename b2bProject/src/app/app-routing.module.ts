6ef4d666c8504fd9061eddf6847f66fcaf467ae7
NotMeAgain10 <dev@isupport.com.gr>
2022-12-06 17:00:07 +0200

    lalal

--- b2bProject/src/app/app-routing.module.ts
+++ b2bProject/src/app/app-routing.module.ts
@@ -1,105 +1,119 @@
-import { Component, NgModule } from "@angular/core";
-import { Routes,RouterModule } from "@angular/router";
-import { AdminAreaComponent } from "./AdminArea/admin-area/admin-area.component";
-import { AdminareaproductsComponent } from "./AdminArea/adminareaproducts/adminareaproducts.component";
-import { AdminareausersComponent } from "./AdminArea/adminareausers/adminareausers.component";
-import { CatalogsComponent } from "./AdminArea/catalogs/catalogs.component";
-import { DeleteCatalogsComponent } from "./AdminArea/components/delete-catalogs/delete-catalogs.component";
-import { HomepageCarouselComponent } from "./AdminArea/components/homepage-carousel/homepage-carousel.component";
-import { InsertImagesComponent } from "./AdminArea/components/insert-images/insert-images.component";
-import { InsertPdfComponent } from "./AdminArea/components/insert-pdf/insert-pdf.component";
-import { InsertProductsComponent } from "./AdminArea/components/insert-products/insert-products.component";
-import { UpdateCategoriesComponent } from "./AdminArea/components/update-categories/update-categories.component";
-import { UpdateColorsComponent } from "./AdminArea/components/update-colors/update-colors.component";
-import { UpdateUsersComponent } from "./AdminArea/components/update-users/update-users.component";
-import { UploadCatalogsComponent } from "./AdminArea/components/upload-catalogs/upload-catalogs.component";
-import { UploadImageComponent } from "./AdminArea/components/upload-image/upload-image.component";
-import { UploadPdfComponent } from "./AdminArea/components/upload-pdf/upload-pdf.component";
-import { ImageComponent } from "./AdminArea/image/image.component";
-import { PdfComponent } from "./AdminArea/pdf-component/pdf-component.component";
-import { CartComponent } from "./cart/cart.component";
-import { FavoriteComponent } from "./cart/favorite/favorite.component";
-import { CatalogsFrontComponent } from "./catalogs-front/catalogs-front.component";
-import { CheckoutPageComponent } from "./checkout-page/checkout-page.component";
-import { HomepageComponent } from "./homepage/homepage.component";
-import { LogInComponent } from "./log-in/log-in.component";
-import { MyOrdersComponent } from "./my-orders/my-orders.component";
-import { OrderCompletedComponent } from "./order-completed/order-completed.component";
-import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
-import { MosquiWizzardComponent } from "./porducts/mosqui-wizzard/mosqui-wizzard.component";
-import { PorductsComponent } from "./porducts/porducts.component";
-import { ProductListComponent } from "./porducts/product-list/product-list.component";
-import { ProductPageComponent } from "./porducts/product-page/product-page.component";
-import { ProfileComponent } from "./profile/profile.component";
-import { AdminGuard } from "./services/admin.guard";
-import { AuthGuard } from "./services/auth.guard";
-import { ContatcFormComponent } from "./shared/contatc-form/contatc-form.component";
-import { TeamComponent } from "./team/team.component";
-import { TheCompanyComponent } from "./the-company/the-company.component";
+import { Component, NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AdminAreaComponent } from './AdminArea/admin-area/admin-area.component';
+import { AdminareaproductsComponent } from './AdminArea/adminareaproducts/adminareaproducts.component';
+import { AdminareausersComponent } from './AdminArea/adminareausers/adminareausers.component';
+import { CatalogsComponent } from './AdminArea/catalogs/catalogs.component';
+import { DeleteCatalogsComponent } from './AdminArea/components/delete-catalogs/delete-catalogs.component';
+import { HomepageCarouselComponent } from './AdminArea/components/homepage-carousel/homepage-carousel.component';
+import { InsertImagesComponent } from './AdminArea/components/insert-images/insert-images.component';
+import { InsertPdfComponent } from './AdminArea/components/insert-pdf/insert-pdf.component';
+import { InsertProductsComponent } from './AdminArea/components/insert-products/insert-products.component';
+import { UpdateCategoriesComponent } from './AdminArea/components/update-categories/update-categories.component';
+import { UpdateColorsComponent } from './AdminArea/components/update-colors/update-colors.component';
+import { UpdateUsersComponent } from './AdminArea/components/update-users/update-users.component';
+import { UploadCatalogsComponent } from './AdminArea/components/upload-catalogs/upload-catalogs.component';
+import { UploadImageComponent } from './AdminArea/components/upload-image/upload-image.component';
+import { UploadPdfComponent } from './AdminArea/components/upload-pdf/upload-pdf.component';
+import { ImageComponent } from './AdminArea/image/image.component';
+import { PdfComponent } from './AdminArea/pdf-component/pdf-component.component';
+import { CartComponent } from './cart/cart.component';
+import { FavoriteComponent } from './cart/favorite/favorite.component';
+import { CatalogsFrontComponent } from './catalogs-front/catalogs-front.component';
+import { CheckoutPageComponent } from './checkout-page/checkout-page.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { LogInComponent } from './log-in/log-in.component';
+import { MyOrdersComponent } from './my-orders/my-orders.component';
+import { OrderCompletedComponent } from './order-completed/order-completed.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { MosquiWizzardComponent } from './porducts/mosqui-wizzard/mosqui-wizzard.component';
+import { PorductsComponent } from './porducts/porducts.component';
+import { ProductListComponent } from './porducts/product-list/product-list.component';
+import { ProductPageComponent } from './porducts/product-page/product-page.component';
+import { ProfileComponent } from './profile/profile.component';
+import { AdminGuard } from './services/admin.guard';
+import { AuthGuard } from './services/auth.guard';
+import { ContatcFormComponent } from './shared/contatc-form/contatc-form.component';
+import { TeamComponent } from './team/team.component';
+import { TheCompanyComponent } from './the-company/the-company.component';
 
-const routes :Routes =[
+const routes: Routes = [
   {
-    path:'',
-    pathMatch:'full',
-    redirectTo:'home'
-  }, {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home',
+  },
+  {
     path: 'log-in',
-    component: LogInComponent
+    component: LogInComponent,
   },
   {
-    path:'home',
-    component:HomepageComponent
+    path: 'home',
+    component: HomepageComponent,
   },
   {
-    path:'catalogs',
-    component: CatalogsFrontComponent
+    path: 'catalogs',
+    component: CatalogsFrontComponent,
   },
   {
-    path:'products',
-    component:PorductsComponent,
-    data: {breadcrumb: {alias: 'Προϊόντα'}},
+    path: 'products',
+    component: PorductsComponent,
+    data: { breadcrumb: { alias: 'Προϊόντα' } },
     children: [
-      {path: ':cat_id/:cat_name', component: ProductListComponent, pathMatch: 'full',data: {breadcrumb: {alias: ':cat_name'}},},
-      {path: 'product-page', component: ProductPageComponent,children: [
-        {
-          path: '**',
-          component: ProductPageComponent
-        }
-      ]},
-      {path: 'mosqui/:sub_id/:sub_name',data: {breadcrumb: {alias: 'Mosqui'}},component:MosquiWizzardComponent}
-    ]
+      {
+        path: ':cat_id/:cat_name',
+        component: ProductListComponent,
+        pathMatch: 'full',
+        data: { breadcrumb: { alias: ':cat_name' } },
+      },
+      {
+        path: 'product-page',
+        component: ProductPageComponent,
+        children: [
+          {
+            path: '**',
+            component: ProductPageComponent,
+          },
+        ],
+      },
+      {
+        path: 'mosqui/:sub_id/:sub_name',
+        data: { breadcrumb: { alias: 'Mosqui' } },
+        component: MosquiWizzardComponent,
+      },
+    ],
   },
   {
-    path:'dashboard',
-    canActivate:[AdminGuard],
-    component:AdminAreaComponent,
-    children:[
+    path: 'dashboard',
+    canActivate: [AdminGuard],
+    component: AdminAreaComponent,
+    children: [
       {
         path: 'insert-products',
-        component: InsertProductsComponent
+        component: InsertProductsComponent,
       },
 
       {
         path: 'update-categories',
-        component: UpdateCategoriesComponent
+        component: UpdateCategoriesComponent,
       },
       {
         path: 'update-colors',
-        component: UpdateColorsComponent
+        component: UpdateColorsComponent,
       },
       {
         path: 'pdf',
         component: PdfComponent,
-        children:[
+        children: [
           {
             path: 'upload',
-            component: UploadPdfComponent
+            component: UploadPdfComponent,
           },
           {
             path: 'insert',
-            component: InsertPdfComponent
-          }
-        ]
+            component: InsertPdfComponent,
+          },
+        ],
       },
       {
         path: 'image',
@@ -107,17 +121,17 @@ const routes :Routes =[
         children: [
           {
             path: 'upload',
-            component: UploadImageComponent
+            component: UploadImageComponent,
           },
           {
             path: 'insert',
-            component: InsertImagesComponent
-          }
-        ]
+            component: InsertImagesComponent,
+          },
+        ],
       },
       {
         path: 'carousel',
-        component: HomepageCarouselComponent
+        component: HomepageCarouselComponent,
       },
       {
         path: 'catalogs',
@@ -125,80 +139,76 @@ const routes :Routes =[
         children: [
           {
             path: 'upload',
-            component: UploadCatalogsComponent
+            component: UploadCatalogsComponent,
           },
           {
             path: 'delete',
-            component: DeleteCatalogsComponent
-          }
-        ]
+            component: DeleteCatalogsComponent,
+          },
+        ],
       },
       {
-        path:'update-users',
-        component:UpdateUsersComponent
-      }
-    ]
+        path: 'update-users',
+        component: UpdateUsersComponent,
+      },
+    ],
   },
 
   {
     path: 'cart',
     canActivate: [AuthGuard],
-    component: CartComponent
+    component: CartComponent,
   },
   {
-    path:'favorites',
-    component:FavoriteComponent,
-    canActivate:[AuthGuard]
+    path: 'favorites',
+    component: FavoriteComponent,
+    canActivate: [AuthGuard],
   },
 
   {
     path: 'checkout',
-    canActivate:[AuthGuard],
-    component: CheckoutPageComponent
+    canActivate: [AuthGuard],
+    component: CheckoutPageComponent,
   },
   {
     path: 'order-completed',
-    canActivate:[AuthGuard],
-    component: OrderCompletedComponent
+    canActivate: [AuthGuard],
+    component: OrderCompletedComponent,
   },
   {
     path: 'profile',
     canActivate: [AuthGuard],
-    component: ProfileComponent
+    component: ProfileComponent,
   },
   {
     path: 'my-orders',
     canActivate: [AuthGuard],
-    component: MyOrdersComponent
+    component: MyOrdersComponent,
   },
 
   {
-    path:'contact',
-    component:ContatcFormComponent
+    path: 'contact',
+    component: ContatcFormComponent,
   },
   {
     path: 'company',
-    component: TheCompanyComponent
+    component: TheCompanyComponent,
   },
   {
     path: 'the-team',
-    component: TeamComponent
+    component: TeamComponent,
   },
-{
-  path:'**',
-  redirectTo:'404'
-},
-{
-  path:'404',
-  component:PageNotFoundComponent
-}
-
-
-
+  {
+    path: '**',
+    redirectTo: '404',
+  },
+  {
+    path: '404',
+    component: PageNotFoundComponent,
+  },
 ];
 @NgModule({
-  imports:[RouterModule.forRoot(routes, {scrollPositionRestoration: 'top'})],
-  exports:[RouterModule]
+  imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'top' })],
+  exports: [RouterModule],
 })
-
-export class AppRoutingModule{}
+export class AppRoutingModule {}
